ab7f7dae820d1a6e0f13fe7c953f39e182566805
MD AAMIR <mdaamir9110641@gmail.com>
2023-06-17 20:02:56 -0700

    social app

---
+++ app.js
@@ -0,0 +1,59 @@
+function signup(){
+console.log("hello world")
+var fullName = document.getElementById("fullName").value;
+var phoneNumber = document.getElementById("phoneNumber").value;
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+
+var userObj = {
+fullName,
+phoneNumber,
+email,
+password,
+}
+var getUsers = JSON.parse(localStorage.getItem("users"))
+
+if(getUsers === null){
+var array = [];
+array.push(userObj)
+localStorage.setItem("users", JSON.stringify(array))
+alert("user signup")
+
+
+
+}
+else{
+var findUser = getUsers.find(function(value){
+if (value.email === email) {
+    return true
+}
+
+
+
+})
+
+if (findUser === undefined) {
+    getUsers.push(userObj)
+    localStorage.setItem("users", JSON.stringify(getUsers))
+    alert("user signup")
+   window.location.href = "./index.html"
+}
+else{
+    alert("email address already exists")
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
